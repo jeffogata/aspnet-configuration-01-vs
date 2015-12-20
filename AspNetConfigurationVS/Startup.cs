39c0bf2f38b050336c789643c6a390048debcd60
Jeff Ogata <jeffogata@outlook.com>
2015-12-20 17:49:53 -0600

    initial commit

---
+++ AspNetConfigurationVS/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace AspNetConfigurationVS
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.Configuration;
+
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            var setting = _configuration["setting"];
+            var defaultConnection = _configuration["data:connectionStrings:default"];
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(
+                    $"<div>Setting = {setting}</div>" +
+                    $"<div>Default Connection = {defaultConnection}</div>");
+            });
+        }
+    }
+}
